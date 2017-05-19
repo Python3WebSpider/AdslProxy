0293bb64a8f49b819a69792ddb9408d95f1e1ed3
Germey <cqc@cuiqingcai.com>
2017-05-19 14:57:04 +0800

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,82 @@
-# ADSLProxyPool
+# ADSL拨号服务器代理设置
 
-ADSLProxyPool
+## 服务端
+
+服务端即远程主机
+
+### 安装Python
+
+Python3版本
+
+
+### 安装库
+
+```
+pip3 install redis tornado
+```
+
+### Clone项目
+
+```
+git clone https://github.com/Germey/ADSLProxyPool.git
+```
+
+### 安装Redis
+
+安装Redis并启动服务
+
+### 修改配置
+
+```
+cd server
+vi config.py
+```
+
+根据注释修改配置文件
+
+### 运行
+
+根目录运行
+
+```
+python3 server.py
+```
+
+## 客户端
+
+客户端即拨号主机
+
+### 安装Python
+
+Python3版本
+
+
+### 安装库
+
+```
+pip3 install requests
+```
+
+### Clone项目
+
+```
+git clone https://github.com/Germey/ADSLProxyPool.git
+```
+
+
+### 修改配置
+
+```
+cd server
+vi config.py
+```
+
+根据注释修改配置文件
+
+### 运行
+
+根目录运行
+
+```
+python3 client.py
+```
--- server/db.py
+++ server/db.py
@@ -23,10 +23,6 @@ class RedisClient(object):
     def remove(self, name):
         return self.db.delete(self.key(name))
 
-    def random(self):
-        items = self.all()
-        return random.choice(items).get('proxy')
-
     def keys(self):
         return [key.decode('utf-8').replace(self.proxy_key + ':', '') for key in self.db.keys(self.key('*'))]
 
@@ -34,6 +30,10 @@ class RedisClient(object):
         keys = self.keys()
         proxies = [{'name': key, 'proxy': self.get(key)} for key in keys]
         return proxies
+
+    def random(self):
+        items = self.all()
+        return random.choice(items).get('proxy')
 
     def list(self):
         keys = self.keys()
@@ -55,3 +55,4 @@ if __name__ == '__main__':
     print(client.count())
     print(client.all())
     print(result)
+
