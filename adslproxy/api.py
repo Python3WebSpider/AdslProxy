8fe3dd75a72c4e8d8e19025004dcc9fef57c0c54
Germey <cqc@cuiqingcai.com>
2017-07-14 15:49:00 +0800

    update list

--- adslproxy/api.py
+++ adslproxy/api.py
@@ -5,38 +5,38 @@ import tornado.web
 from tornado.web import RequestHandler, Application
 from adslproxy.config import *
 
+
 class MainHandler(RequestHandler):
     def initialize(self, redis):
         self.redis = redis
-
+    
     def get(self, api=''):
         if not api:
-            links = ['first', 'random', 'list', 'all', 'count']
+            links = ['random', 'proxies', 'names', 'all', 'count']
             self.write('<h4>Welcome to ADSL Proxy API</h4>')
             for link in links:
                 self.write('<a href=' + link + '>' + link + '</a><br>')
-
-        if api == 'first':
-            result = self.redis.first()
-            if result:
-                self.write(result)
-
+        
         if api == 'random':
             result = self.redis.random()
             if result:
                 self.write(result)
-
-        if api == 'list':
-            result = self.redis.list()
+        
+        if api == 'names':
+            result = self.redis.names()
             if result:
-                for proxy in result:
-                    self.write(proxy + '<br>')
-
+                self.write(json.dumps(result))
+        
+        if api == 'proxies':
+            result = self.redis.proxies()
+            if result:
+                self.write(json.dumps(result))
+        
         if api == 'all':
             result = self.redis.all()
             if result:
                 self.write(json.dumps(result))
-
+        
         if api == 'count':
             self.write(str(self.redis.count()))
 
@@ -49,4 +49,3 @@ def server(redis, port=API_PORT, address=''):
     application.listen(port, address=address)
     print('ADSL API Listening on', port)
     tornado.ioloop.IOLoop.instance().start()
-
